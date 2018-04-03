a8646a57cbaf326c33824270cd3b4f9959c6e664
Ryan Pencak <rvp002@bucknell.edu>
2018-04-03 19:24:14 -0400

    compile third test

--- test/third_test/third_test.c
+++ test/third_test/third_test.c
@@ -1,35 +1,35 @@
-// Third test written in C to include 28 MIPS instructions
-
-#include <stdio.h>
-
 extern int puts(const char *str);
-
-int third_test(int x)
-{
-   x *= 10;
-   x /= 10;
-   x -= 10;
-   x += 10;
-
-   if ( x % 10 == 0) {
-     int y = 2;
-   }
-   else {
-     int y = 1;
-   }
-
-   for (int i=0; i<y; i++) {
-     x += i;
-   }
-
-   return x;
-
-}
-
-int main(void)
-{
-    int test_output = third_test(10);
-    puts("Result is:");
-    puts(test_output); // output should be
-    puts("\n");
-}
+extern char* third_test(int);
+ 
+ char* third_test(int x)
+ {
+    x *= 10;
+    x /= 10;
+    x -= 10;
+    x += 10;
+    char* out[20];
+    int i;
+    int y;
+    
+    if ( x % 10 == 0) {
+      y = 2; 
+    } 
+    else {
+      y = 1;
+    } 
+    
+    for (i=0; i<y; i++) {
+      x += i;
+    } 
+    
+    if( x == 11) {
+        puts("correct!");
+    }   
+    
+ }  
+ 
+ int main(void)
+ {
+     char* test_output = third_test(10);
+     puts("\n");
+ }
