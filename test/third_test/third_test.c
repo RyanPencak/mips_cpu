48cb9da0ff16d8b2d388f2ab8fce8f6eee1c897c
Ryan Pencak <rvp002@bucknell.edu>
2018-04-03 19:33:28 -0400

    Revised Test 3

--- test/third_test/third_test.c
+++ test/third_test/third_test.c
@@ -3,25 +3,33 @@ extern char* third_test(int);
  
  char* third_test(int x)
  {
-    x *= 10;
+    x = x * 10;
+    x = x * 1;
     x /= 10;
     x -= 10;
     x += 10;
     char* out[20];
     int i;
     int y;
-    
-    if ( x % 10 == 0) {
+   
+    if ((x==9) || (x==8)) {
+        y = 1;
+    }
+    if ((x==9) ^ (x==8)) {
+        y = 1;
+    }
+    if (x % 10 != 0) {
+        y = 1;
+    }
+    else if ( x % 10 == 0) {
       y = 2; 
     } 
     else {
       y = 1;
     } 
-    
     for (i=0; i<y; i++) {
       x += i;
     } 
-    
     if( x == 11) {
         puts("correct!");
     }   
